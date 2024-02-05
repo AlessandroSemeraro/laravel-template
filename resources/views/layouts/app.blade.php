0c6184efde19c7a581b301b73a85ce733dddd09c
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-05 21:38:11 +0100

    added partials (header,footer)to template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Laravel')</title>
     @vite('resources/js/app.js')
+
+    @yield('head-content')
 </head>
 <body>
-  
+    @include('partials.header')
+
+    <main>
+        @yield('main-content')
+    </main>
+
+    @include('partials.footer')
+
+    @yield('script-content')
 </body>
 </html>
